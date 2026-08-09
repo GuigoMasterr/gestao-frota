86188a7868b3f34b70a0c7aad0c69f6f4b565b89
GuigoMaster <igorss2806@gmail.com>
2026-08-09 11:27:59 -0400

    Corrige porta do banco para usar variavel de ambiente

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -9,7 +9,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.db = new pg_1.Pool({
     host: process.env.DB_HOST,
-    port: 5432,
+    port: parseInt(process.env.DB_PORT || '5432'),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const db = new Pool({
   host: process.env.DB_HOST,
-  port: 5432,
+  port: parseInt(process.env.DB_PORT || '5432'),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
